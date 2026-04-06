6cd4f009cea4e49eaff25983c6672e55264b7f23
Anish2230 <anishpande1234@gmail.com>
2026-04-06 21:54:48 +0530

    add health route + remove frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: "https://finance-manager-backend-65n8.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
